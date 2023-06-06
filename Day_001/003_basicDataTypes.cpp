56748a38a108175d674fe0e0daf8b2cc0609857a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 06:48:16 +0530

    code added

---
+++ Day_001/003_basicDataTypes.cpp
@@ -0,0 +1,21 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    
+    cout << a <<"\n" << b <<"\n" << c <<"\n" << d <<"\n" << e;
+    
+    return 0;
+}
