fcee4f39af8b37e4d445c298b0d9b93596aea881
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 13:08:02 +0000

    code added

---
+++ 0007_summationFrom1ToN.cpp
@@ -0,0 +1,15 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    
+    cout << (n * (n + 1) / 2);
+    
+    return 0;
+}
