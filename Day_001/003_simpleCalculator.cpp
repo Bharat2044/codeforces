3654fe1e2ea7824038ac78db05486be786c755c6
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 06:50:14 +0530

    code added

---
+++ Day_001/003_simpleCalculator.cpp
@@ -0,0 +1,18 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a, b;
+    cin >> a >> b;
+    
+    cout << a <<" + " << b <<" = " << (a + b) <<"\n";
+    cout << a <<" * " << b <<" = " << (a * b) <<"\n";
+    cout << a <<" - " << b <<" = " << (a - b) <<"\n";
+    
+    return 0;
+}
