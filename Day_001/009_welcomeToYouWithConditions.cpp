a24add0ffae6268ab76164f7033925ce6ec2d3db
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 06:58:22 +0530

    code added

---
+++ Day_001/009_welcomeToYouWithConditions.cpp
@@ -0,0 +1,15 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    (a >= b) ? cout << "Yes\n" : cout << "No\n";
+    
+    return 0;
+}
