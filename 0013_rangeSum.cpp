94852850afd81f6f700193d6d1b7dc1e5ac5d07a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 13:08:33 +0000

    code added

---
+++ 0013_rangeSum.cpp
@@ -0,0 +1,58 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/D
+
+
+// Approach - 1: TC = O(t * max(l, r)), SC = O(1)
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        long long l, r;
+        cin >> l >> r;
+        
+        if(l > r)
+            swap(l, r);
+            
+        long long sum = 0;
+            
+        for(int i=l; i<=r; i++)
+            sum += i;
+        
+        cout << sum << "\n";
+    }
+    
+    return 0;
+}
+*/
+
+
+// Approach - 2: TC = O(t), SC = O(1)
+#include <iostream>
+using namespace std;
+
+long long sum(int n) {
+    return (n * 1ll * (n + 1) / 2) ;  
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        long long l, r;
+        cin >> l >> r;
+        
+        if(l > r)
+            swap(l, r);
+            
+        long long ans = sum(r) - sum(l-1);
+        
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
