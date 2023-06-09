3758e42990aff5c771f4c186dae66cd753c330e1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-09 08:16:56 +0530

    code added

---
+++ 0028_interval.cpp
@@ -0,0 +1,27 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/S
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+
+int main () {
+    float n;
+    cin >> n;	
+
+
+    if (n < 0 || n > 100)
+      cout << "Out of Intervals" << endl;
+      
+    else {
+        if (n >= 0 && n <= 25)
+	        cout << "Interval [0,25]" << endl;
+        else if (n > 25 && n <= 50)
+	        cout << "Interval (25,50]" << endl;
+        else if (n > 50 && n <= 75)
+	        cout << "Interval (50,75]" << endl;
+        else
+	        cout << "Interval (75,100]" << endl;
+    }
+
+    return 0;
+}
