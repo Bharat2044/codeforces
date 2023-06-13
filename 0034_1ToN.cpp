6b5d3cf836870e3d66c6d2f870e760d2f8f69a28
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-13 14:01:12 +0530

    code added

---
+++ 0034_1ToN.cpp
@@ -0,0 +1,19 @@
+// Question Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+
+
+// TC = O(n), SC = O(1)
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++) 
+        cout << i << "\n";
+
+    return 0;
+}
