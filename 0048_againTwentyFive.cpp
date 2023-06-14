e75605f5a4776ebb743491a373d324badbf9b2fa
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-14 16:13:17 +0530

    code added

---
+++ 0048_againTwentyFive.cpp
@@ -0,0 +1,21 @@
+// Question Link: https://codeforces.com/problemset/problem/630/A
+
+
+// TC = O(1), SC = O(1)
+#include <iostream>
+using namespace std;
+
+#define endl '\n'
+const int MOD = 1e9+7;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n;
+    cin >> n;
+    
+    cout << 25;
+
+    return 0;
+}
