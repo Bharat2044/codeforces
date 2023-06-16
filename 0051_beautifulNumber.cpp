d0ac08f246c3d06b3b29531de06e7ef5cd0bb0cb
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-16 19:09:19 +0530

    code added

--- 0051_beautifulNumber.cpp
+++ 0051_beautifulNumber.cpp
@@ -1,17 +1,94 @@
 // Question Link: https://codeforces.com/problemset/problem/271/A
 
 
-// TC = O(log(y)), SC = O(1)
+// TC = O(1), SC = O(1)
 #include <iostream>
-#include <set>
 using namespace std;
 
-void solve(int y) {
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int y;
+    cin >> y;
+    
+    y++;
+    
+    while(true) {
+        int a = y%10;
+        int b = (y%100)/10;
+        int c = (y%1000)/100;
+        int d = y/1000;
+        
+        if(a==b || a==c || a==d || b==c || b==d || c==d) 
+            y++;
+        else
+            break;
+    }
+    
+    cout << y << "\n";
+    
+    return 0;
+}
+
+
+// TC = O(log(y)*log(y)), SC = O(1)
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int y;
+    cin >> y;
+    
     y++;
     
     while(true) {
         string str = to_string(y);
-      
+        bool flag = true;
+        
+        for(int i=0; i<str.size(); i++) {
+            for(int j=i+1; j<str.size(); j++) {
+                if(str[i] == str[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        
+        if(flag) {
+            cout << y << "\n";
+            break;
+        }
+        
+        y++;
+    }
+    
+    return 0;
+}
+*/
+
+
+// TC = O(log(y)), SC = O(1)
+/*
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int y;
+    cin >> y;
+    
+    y++;
+    
+    while(true) {
+        string str = to_string(y);
         set<char> st;
         bool flag = true;
         
@@ -25,21 +102,12 @@ void solve(int y) {
         
         if(flag) {
             cout << y << "\n";
-            return;
+            break;
         }
         
         y++;
     }
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     
-    int y;
-    cin >> y;
-    
-    solve(y);
-
     return 0;
 }
+*/
